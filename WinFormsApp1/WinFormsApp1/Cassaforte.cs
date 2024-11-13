3d8ac77bce397231385b78e293e49618a63e342a
MAFFEIS MATTIA <MAFFEIS.21196>
2024-11-13 13:43:16 +0100

    priva versione

---
+++ WinFormsApp1/WinFormsApp1/Cassaforte.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsApp1
+{
+    internal class Cassaforte
+    { 
+        private string matricola;
+        private string produttore;
+        private string modello;
+        private string codiceSbloccoU;
+        private bool stato;
+        private bool bloccato;
+        private string cUtente;
+        private int provaSblocco;
+        public string Matricola
+        {
+            get { return matricola; }
+        }
+        public string Produttore
+        {
+            get { return produttore; }
+        }
+        public string Modello
+        {
+            get { return modello; }
+        }
+        private string codiceSbloccoU;
+        public string CodiceSbloccoU
+        {
+            get { return codiceSbloccoU; }
+        }
+        public bool Stato
+        {
+            get { return stato; }
+            set { stato = value; }
+        }
+        public bool Bloccato
+        {
+            get { return bloccato; }
+            set { bloccato = value; }
+        }
+        public string Cutente
+        {
+            get { return cUtente; }
+        }
+        public int Provasblocco
+        {
+            get { return provaSblocco; }
+            set { provaSblocco = value; }
+        }
+
+
+        public Cassaforte(string matricola, string produttore, string modello, string codiceSbloccou)
+        {
+            this.matricola = matricola;
+            this.produttore = produttore;
+            this.modello = modello;
+            if (codiceSbloccoU.Length != 12)
+                return;
+            else
+                this.codiceSbloccoU = codiceSbloccou;
+            this.stato = false;
+            this.bloccato = false;
+        }
+
+        public void ImpostaCodiceU(string codice)
+        {
+            if (codice.Length != 5)
+                return;
+            else
+                cUtente = codice;
+        }
+
+        public void CassaforteAperta(string codice)
+        {
+            if (bloccato == true)
+                return;
+            if (codice != cUtente)
+            {
+                provaSblocco++;
+                if (provaSblocco == 4)
+                   bloccato = true;
+                return;
+            }
+            Stato = true;
+        }
+
+        public void ChiudiCassaforte()
+        {
+            Stato = false;
+        }
+
+        public void SbloccaCassaforte(string codice)
+        {
+            if (codice != codiceSbloccoU)
+                return;
+            bloccato = false;
+            provaSblocco = 0;
+            Stato = true;
+        }
+    }
+
+}
---
+++ WinFormsApp1/WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            label3 = new Label();
+            label4 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            label5 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(445, 89);
+            label1.Name = "label1";
+            label1.Size = new Size(86, 15);
+            label1.TabIndex = 1;
+            label1.Text = "codice sblocco";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(149, 89);
+            label2.Name = "label2";
+            label2.Size = new Size(90, 15);
+            label2.TabIndex = 2;
+            label2.Text = "CODICEUTENTE";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(139, 135);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(431, 135);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(100, 23);
+            textBox2.TabIndex = 5;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(278, 258);
+            label3.Name = "label3";
+            label3.Size = new Size(33, 15);
+            label3.TabIndex = 6;
+            label3.Text = "stato";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(278, 289);
+            label4.Name = "label4";
+            label4.Size = new Size(76, 15);
+            label4.TabIndex = 7;
+            label4.Text = "CASSAFORTE";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(264, 134);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 8;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(591, 135);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 9;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(317, 258);
+            label5.Name = "label5";
+            label5.Size = new Size(38, 15);
+            label5.TabIndex = 10;
+            label5.Text = "label5";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label5);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Label label3;
+        private Label label4;
+        private Button button1;
+        private Button button2;
+        private Label label5;
+    }
+}
